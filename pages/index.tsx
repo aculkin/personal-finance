97f29e5017acc9cd6292cc4cb56de330eeef7182
aculkin <andrew.culkin@hover.to>
2023-01-09 11:16:03 -0500

    feat: import default example data

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,11 @@
+import { Button } from '@mui/material'
 import Head from 'next/head'
+import '@fontsource/roboto/300.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+
+import * as accounts from '../public/exampleData/accounts.json'
 
 export default function Home() {
   return (
@@ -10,6 +17,12 @@ export default function Home() {
 
       <main>
         <h1>Welcome to my finance app</h1>
+        {accounts.map(({ id }) => (
+          <div>
+            <p>account {id}</p>
+            <Button variant="contained">My button</Button>
+          </div>
+        ))}
       </main>
     </div>
   )
