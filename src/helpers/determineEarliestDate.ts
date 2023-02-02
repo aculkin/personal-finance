1075975f330a9980468548341d1312ced9ad7fb4
aculkin <andrew.culkin@hover.to>
2023-02-02 11:52:30 -0500

    remove unused function

--- src/helpers/determineEarliestDate.ts
+++
@@ -1,8 +0,0 @@
-import { min } from 'date-fns'
-import { Balances } from '../../types'
-const determineEarliestDate = (balances: Balances[]) => {
-  let dateArray = balances.map((balance) => new Date(balance.date))
-  return min(dateArray)
-}
-
-export { determineEarliestDate }
