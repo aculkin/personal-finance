bda4a16b3cd8bb03350ffbe14909e864a07790ad
aculkin <andrew.culkin@hover.to>
2023-03-05 15:21:15 -0500

    feat: fix date and account selection in forms

--- src/components/forms/BalanceForm.tsx
+++ src/components/forms/BalanceForm.tsx
@@ -1,60 +1,73 @@
-import { format } from 'date-fns'
 import {
-  FormControl,
-  NumberInput,
-  NumberInputField,
-  Input,
-  FormLabel,
-  InputGroup,
-  InputLeftAddon,
-} from '@chakra-ui/react'
+	FormControl,
+	NumberInput,
+	NumberInputField,
+	FormLabel,
+	InputGroup,
+	InputLeftAddon,
+	Select,
+} from "@chakra-ui/react";
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
+
+import { useAppState } from "../../hooks";
 
 interface BalanceForm {
-  date?: Date
-  amount?: number
-  accountId?: number
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+	date?: Date;
+	amount?: number;
+	accountId?: number;
+	handleChange: (
+		event:
+			| React.ChangeEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLSelectElement>
+	) => void;
 }
 
 const BalanceForm = ({
-  date,
-  amount,
-  accountId,
-  handleChange,
+	date,
+	amount,
+	accountId,
+	handleChange,
 }: BalanceForm) => {
-  return (
-    <FormControl alignItems="center">
-      <FormLabel mb="0">Amount:</FormLabel>
-      <InputGroup>
-        <InputLeftAddon children="#" />
-        <NumberInput width="100%">
-          <NumberInputField
-            value={amount}
-            name="amount"
-            onChange={handleChange}
-          />
-        </NumberInput>
-      </InputGroup>
-      <FormLabel mb="0">Account Id</FormLabel>
-      <InputGroup>
-        <InputLeftAddon children="#" />
-        <NumberInput width="100%">
-          <NumberInputField
-            value={accountId}
-            name="accountId"
-            onChange={handleChange}
-          />
-        </NumberInput>
-      </InputGroup>
-      <FormLabel mb="0">Balance Date:</FormLabel>
-      <Input
-        type="date"
-        name="date"
-        onChange={handleChange}
-        value={date ? format(date, 'yyyy-MM-dd') : null}
-      />
-    </FormControl>
-  )
-}
+	const { accounts } = useAppState();
+	const accountList = [{ id: null, name: "N/A" }, ...accounts];
+	return (
+		<FormControl alignItems='center'>
+			<FormLabel mb='0'>Account</FormLabel>
+			<Select name='accountId' value={accountId || ""} onChange={handleChange}>
+				{accountList.map(({ id, name }) => (
+					<option key={id} value={id}>
+						{name}
+					</option>
+				))}
+			</Select>
+			<FormLabel mb='0'>Amount:</FormLabel>
+			<InputGroup>
+				<InputLeftAddon children='$' />
+				<NumberInput width='100%'>
+					<NumberInputField
+						placeholder='500.00'
+						value={amount}
+						name='amount'
+						onChange={handleChange}
+					/>
+				</NumberInput>
+			</InputGroup>
+			<FormLabel mb='0'>Balance Date:</FormLabel>
+			<SingleDatepicker
+				date={date}
+				name='date'
+				onDateChange={(newDate: Date) =>
+					handleChange({
+						target: {
+							name: "date",
+							// @ts-expect-error
+							value: newDate,
+						},
+					})
+				}
+			/>
+		</FormControl>
+	);
+};
 
-export { BalanceForm }
+export { BalanceForm };
--- src/components/forms/TransactionForm.tsx
+++ src/components/forms/TransactionForm.tsx
@@ -1,104 +1,131 @@
-import { format } from 'date-fns'
+import { format } from "date-fns";
 import {
-  FormControl,
-  Select,
-  NumberInput,
-  NumberInputField,
-  Input,
-  Switch,
-  FormLabel,
-} from '@chakra-ui/react'
+	FormControl,
+	Select,
+	NumberInput,
+	NumberInputField,
+	Input,
+	Switch,
+	FormLabel,
+	InputGroup,
+} from "@chakra-ui/react";
+import { SingleDatepicker } from "chakra-dayzed-datepicker";
+
+import { useAppState } from "../../hooks";
+
+const options = {};
 
 interface TransactionForm {
-  name?: string
-  frequency?: string
-  interestRate?: number
-  amount?: number
-  payoffToAccount?: boolean
-  priority?: number
-  fromAccountId?: number
-  toAccountId?: number
-  startDate?: Date
-  handleChange: (event: React.ChangeEvent<HTMLInputElement>) => void
+	name?: string;
+	frequency?: string;
+	interestRate?: number;
+	amount?: number;
+	payoffToAccount?: boolean;
+	priority?: number;
+	fromAccountId?: number;
+	toAccountId?: number;
+	startDate?: Date;
+	handleChange: (
+		event:
+			| React.ChangeEvent<HTMLInputElement>
+			| React.ChangeEvent<HTMLSelectElement>
+	) => void;
 }
 
 const TransactionForm = ({
-  name,
-  frequency,
-  interestRate,
-  amount,
-  payoffToAccount,
-  priority,
-  fromAccountId,
-  toAccountId,
-  startDate = new Date(),
-  handleChange,
+	name,
+	frequency,
+	interestRate,
+	amount,
+	payoffToAccount,
+	priority,
+	fromAccountId,
+	toAccountId,
+	startDate = new Date(),
+	handleChange,
 }: TransactionForm) => {
-  return (
-    <FormControl alignItems="center">
-      <FormLabel mb="0">Transaction Name:</FormLabel>
-      <Input value={name} onChange={handleChange} name="name" />
-      <FormLabel mb="0">Frequency</FormLabel>
-      <Select value={frequency}>
-        <option value="daily">Daily</option>
-        <option value="weekly">Weekly</option>
-        <option value="biWeekly">Bi-Weekly</option>
-        <option value="semiMonthly">Semi-Monthly</option>
-        <option value="monthly">Monthly</option>
-        <option value="daily">Quarterly</option>
-        <option value="daily">Yearly</option>
-      </Select>
-      <FormLabel mb="0">Interest Rate:</FormLabel>
-      <NumberInput>
-        <NumberInputField
-          value={interestRate}
-          onChange={handleChange}
-          name="interestRate"
-        />
-      </NumberInput>
-      <FormLabel mb="0">Amount:</FormLabel>
-      <NumberInput>
-        <NumberInputField
-          value={amount}
-          onChange={handleChange}
-          name="amount"
-        />
-      </NumberInput>
-      <FormLabel mb="0">Payoff the account:</FormLabel>
-      <Switch checked={payoffToAccount} name="payoffToAccount" />
-      <FormLabel mb="0">Priority:</FormLabel>
-      <NumberInput>
-        <NumberInputField
-          value={priority}
-          onChange={handleChange}
-          name="priority"
-        />
-      </NumberInput>
-      <FormLabel mb="0">"From" Account Id</FormLabel>
-      <NumberInput>
-        <NumberInputField
-          value={fromAccountId}
-          onChange={handleChange}
-          name="fromAccountId"
-        />
-      </NumberInput>
-      <FormLabel mb="0">"To" Account Id</FormLabel>
-      <NumberInput>
-        <NumberInputField
-          value={toAccountId}
-          onChange={handleChange}
-          name="toAccountId"
-        />
-      </NumberInput>
-      <FormLabel mb="0">Start Date:</FormLabel>
-      <Input
-        type="date"
-        value={startDate ? format(startDate, 'yyyy-MM-dd') : null}
-        onChange={handleChange}
-        name="startDate"
-      />
-    </FormControl>
-  )
-}
+	const { accounts } = useAppState();
+	const accountList = [{ id: null, name: null }, ...accounts];
+	return (
+		<FormControl alignItems='center'>
+			<FormLabel mb='0'>Transaction Name:</FormLabel>
+			<Input value={name} onChange={handleChange} name='name' />
+			<FormLabel mb='0'>Frequency</FormLabel>
+			<Select value={frequency} onChange={handleChange}>
+				<option value='daily'>Daily</option>
+				<option value='weekly'>Weekly</option>
+				<option value='biWeekly'>Bi-Weekly</option>
+				<option value='semiMonthly'>Semi-Monthly</option>
+				<option value='monthly'>Monthly</option>
+				<option value='daily'>Quarterly</option>
+				<option value='daily'>Yearly</option>
+			</Select>
+			<FormLabel mb='0'>Interest Rate:</FormLabel>
+			<NumberInput>
+				<NumberInputField
+					value={interestRate}
+					onChange={handleChange}
+					name='interestRate'
+				/>
+			</NumberInput>
+			<FormLabel mb='0'>Amount:</FormLabel>
+			<NumberInput>
+				<NumberInputField
+					value={amount}
+					onChange={handleChange}
+					name='amount'
+				/>
+			</NumberInput>
+			<FormLabel mb='0'>Payoff the account:</FormLabel>
+			<Switch checked={payoffToAccount} name='payoffToAccount' />
+			<FormLabel mb='0'>Priority:</FormLabel>
+			<NumberInput>
+				<NumberInputField
+					value={priority}
+					onChange={handleChange}
+					name='priority'
+				/>
+			</NumberInput>
+			<FormLabel mb='0'>"From" Account Id</FormLabel>
+			<Select
+				name='fromAccountId'
+				value={fromAccountId || ""}
+				onChange={handleChange}
+			>
+				{accountList.map(({ id, name }) => (
+					<option key={id} value={id}>
+						{name || "None (external cashflow)"}
+					</option>
+				))}
+			</Select>
+			<FormLabel mb='0'>"To" Account Id</FormLabel>
+			<Select
+				name='toAccountId'
+				value={toAccountId || ""}
+				onChange={handleChange}
+			>
+				{accountList.map(({ id, name }) => (
+					<option key={id} value={id}>
+						{name || "None (external payment)"}
+					</option>
+				))}
+			</Select>
+			<FormLabel mb='0'>Start Date:</FormLabel>
+			<SingleDatepicker
+				date={startDate}
+				name='date'
+				onDateChange={(newDate: Date) =>
+					handleChange({
+						target: {
+							name: "date",
+							// @ts-expect-error
+							value: newDate,
+						},
+					})
+				}
+			/>
+		</FormControl>
+	);
+};
 
-export { TransactionForm }
+export { TransactionForm };
