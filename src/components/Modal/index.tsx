ac5fc894c41a1cf53e9c57dfa3fe5cec3c486c7b
aculkin <andrew.culkin@hover.to>
2023-03-05 15:22:47 -0500

    feat: add cancel and submit text options

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,53 +1,61 @@
 import {
-  Button,
-  Modal as SystemModal,
-  ModalOverlay,
-  ModalContent,
-  ModalHeader,
-  ModalCloseButton,
-  ModalBody,
-  ModalFooter,
-} from '@chakra-ui/react'
-import { ReactNode } from 'react'
+	Button,
+	Modal as SystemModal,
+	ModalOverlay,
+	ModalContent,
+	ModalHeader,
+	ModalCloseButton,
+	ModalBody,
+	ModalFooter,
+} from "@chakra-ui/react";
+import { ReactNode } from "react";
 
 const Modal = ({
-  title,
-  isOpen,
-  handleClose,
-  handleSubmit = () => null,
-  children,
-  isLoading = false,
+	title,
+	isOpen,
+	handleClose,
+	handleSubmit = () => null,
+	children,
+	isLoading = false,
+	confirmText = "Save",
+	cancelText = "Cancel",
 }: {
-  title: string
-  isOpen: boolean
-  handleClose: () => void
-  handleSubmit?: () => void
-  children: ReactNode
-  isLoading?: boolean
+	title: string;
+	isOpen: boolean;
+	handleClose: () => void;
+	handleSubmit?: () => void;
+	children: ReactNode;
+	isLoading?: boolean;
+	confirmText?: string;
+	cancelText?: string;
 }) => {
-  return (
-    <SystemModal isOpen={isOpen} onClose={handleClose}>
-      <ModalOverlay />
-      <ModalContent>
-        <ModalHeader>{title}</ModalHeader>
-        <ModalCloseButton />
-        <ModalBody>{children}</ModalBody>
-        <ModalFooter>
-          <Button
-            isLoading={isLoading}
-            variant="ghost"
-            mr={3}
-            onClick={handleClose}
-          >
-            Cancel
-          </Button>
-          <Button colorScheme="blue" onClick={handleSubmit}>
-            Save
-          </Button>
-        </ModalFooter>
-      </ModalContent>
-    </SystemModal>
-  )
-}
+	return (
+		<SystemModal isOpen={isOpen} onClose={handleClose}>
+			<ModalOverlay />
+			<ModalContent>
+				<ModalHeader>{title}</ModalHeader>
+				<ModalCloseButton />
+				<ModalBody>{children}</ModalBody>
+				<ModalFooter>
+					<Button
+						isLoading={isLoading}
+						variant='ghost'
+						mr={3}
+						onClick={handleClose}
+					>
+						{cancelText}
+					</Button>
+					<Button
+						isLoading={isLoading}
+						colorScheme='blue'
+						onClick={handleSubmit}
+					>
+						{confirmText}
+					</Button>
+				</ModalFooter>
+			</ModalContent>
+		</SystemModal>
+	);
+};
 
-export { Modal }
+export { Modal };
