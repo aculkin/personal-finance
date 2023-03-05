72bcd1e5d2512c99e5f3f33d2c1d320fa9b68800
aculkin <andrew.culkin@hover.to>
2023-03-05 11:10:22 -0500

    feat: add edit and add types

--- types/index.ts
+++ types/index.ts
@@ -1,12 +1,21 @@
-import type { Database } from './supabase'
+import type { Database } from "./supabase";
 
-export type Accounts = Database['public']['Tables']['accounts']['Row'][]
-export type Account = Database['public']['Tables']['accounts']['Row']
-export type Balances = Database['public']['Tables']['balances']['Row'][]
-export type Balance = Database['public']['Tables']['balances']['Row']
-export type Transactions = Database['public']['Tables']['transactions']['Row'][]
-export type Transaction = Database['public']['Tables']['transactions']['Row']
-export type Profiles = Database['public']['Tables']['profiles']['Row'][]
-export type Profile = Database['public']['Tables']['profiles']['Row']
+export type Accounts = Database["public"]["Tables"]["accounts"]["Row"][];
+export type Account = Database["public"]["Tables"]["accounts"]["Row"];
+export type AddAccount = Database["public"]["Tables"]["accounts"]["Insert"];
+export type EditAccount = Database["public"]["Tables"]["accounts"]["Update"];
+export type Balances = Database["public"]["Tables"]["balances"]["Row"][];
+export type Balance = Database["public"]["Tables"]["balances"]["Row"];
+export type AddBalance = Database["public"]["Tables"]["balances"]["Insert"];
+export type EditBalance = Database["public"]["Tables"]["balances"]["Update"];
+export type Transactions =
+	Database["public"]["Tables"]["transactions"]["Row"][];
+export type Transaction = Database["public"]["Tables"]["transactions"]["Row"];
+export type AddTransaction =
+	Database["public"]["Tables"]["transactions"]["Insert"];
+export type EditTransaction =
+	Database["public"]["Tables"]["transactions"]["Update"];
+export type Profiles = Database["public"]["Tables"]["profiles"]["Row"][];
+export type Profile = Database["public"]["Tables"]["profiles"]["Row"];
 
-export default Database
+export default Database;
