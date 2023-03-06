08eca7488e2a1efeaa730a8d65673296834c1715
aculkin <andrew.culkin@hover.to>
2023-03-06 14:25:27 -0500

    feat: add tabs to main page

--- src/layouts/AppLayout.tsx
+++ src/layouts/AppLayout.tsx
@@ -1,12 +1,36 @@
-import { Grid, GridItem, useColorModeValue } from "@chakra-ui/react";
+import {
+	Grid,
+	GridItem,
+	useColorModeValue,
+	Tabs,
+	TabList,
+	Tab,
+} from "@chakra-ui/react";
+import NextLink from "next/link";
+import { useRouter } from "next/router";
 import { ReactNode } from "react";
 
 import { Header } from "../components/Header";
 import { NavBar } from "../components/NavBar";
 import { Footer } from "../components/Footer";
 
+const getTabIndex = (pathname) => {
+	switch (pathname) {
+		case "/accounts":
+			return 1;
+		case "/balances":
+			return 2;
+		case "/transactions":
+			return 3;
+		default:
+			return 0;
+	}
+};
+
 const AppLayout = ({ children }: { children: ReactNode }) => {
 	const formBackground = useColorModeValue("gray.100", "gray.700");
+	const { pathname } = useRouter();
+	const tabIndex = getTabIndex(pathname);
 	return (
 		<Grid
 			templateAreas={`"header header"
@@ -24,8 +48,24 @@ const AppLayout = ({ children }: { children: ReactNode }) => {
 			<GridItem area={"nav"}>
 				<NavBar />
 			</GridItem>
-			<GridItem area={"main"} overflow='scroll'>
-				{children}
+			<GridItem area={"main"} overflow='scroll' padding='10px'>
+				<Tabs variant='enclosed' index={tabIndex}>
+					<TabList>
+						<Tab as={NextLink} href='/'>
+							Account Balances chart
+						</Tab>
+						<Tab as={NextLink} href='/accounts'>
+							Accounts
+						</Tab>
+						<Tab as={NextLink} href='/balances'>
+							Balances
+						</Tab>
+						<Tab as={NextLink} href='/transactions'>
+							Transactions
+						</Tab>
+					</TabList>
+					{children}
+				</Tabs>
 			</GridItem>
 			<GridItem area={"footer"}>
 				<Footer />
